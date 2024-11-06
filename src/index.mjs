865468f8aaa87420747a3b5a23907776fdfc1b75
navin12720 <141739463+navin12720@users.noreply.github.com>
2024-11-06 20:21:06 +0530

    "Updated express-validator import and replaced inline validators with checkSchema in /api/users POST route"

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,39 @@
 import express from "express";
 // const path = require("path");
+import { body, query, validationResult } from "express-validator";
 
 const app = express();
 
 app.use(express.json());
 
+const loggingMiddleware = (req, res, next) => {
+  console.log(`${req.method}--${req.url}`);
+  next();
+};
+
+const resolveUserById = (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  const parseId = parseInt(id);
+  if (isNaN(parseId)) return res.status(400).send("Enter correct Id!");
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parseId);
+  if (findUserIndex === -1) return res.status(404).send("User not found");
+  req.findUserIndex = findUserIndex;
+  next();
+};
+//----------
+app.use(
+  loggingMiddleware, //first this middleware works
+  (req, res, next) => {
+    //then this one works
+    console.log("This is a middleware function");
+    next();
+  }
+); //for all apis
+//------------
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
 const mockUsers = [
   { id: 1, username: "navin", email: "william.on@example-pet-store.com" },
   { id: 2, username: "amer", email: "william2.@example-pet-store.com" },
@@ -24,16 +52,25 @@ const mockUsers = [
 //   .set("view engine", "ejs");
 
 //get request
-app.get("/", (req, res) => {
-  res.send("first page");
-});
+app.get(
+  "/",
+  (req, res, next) => {
+    //middleware
+    console.log("loading finished....");
+    next();
+  },
+  (req, res) => {
+    res.send("first page");
+  }
+);
 
 app.get("/api", (req, res) => {
   res.status(201).send({ msg: "Hel2lo world" });
 });
 
 //get query request
-app.get("/api/users", (req, res) => {
+//express-validator --> query()
+app.get("/api/users", query("filter").isString().notEmpty(), (req, res) => {
   //   res.send(req.query);
   const {
     query: { filter, value },
@@ -44,10 +81,10 @@ app.get("/api/users", (req, res) => {
   //when the filter and value undefined
   return res.send(mockUsers);
 });
-app.get("/api/users/:id", (req, res) => {
-  const parseId = parseInt(req.params.id);
-  if (isNaN(parseId)) return res.status(400).send("Invalid id");
-  const findUser = mockUsers.find((users) => users.id === parseId);
+
+app.get("/api/users/:id", resolveUserById, (req, res) => {
+  const { findUserIndex } = req;
+  const findUser = mockUsers[findUserIndex];
   if (!findUser) return res.status(404).send("User not found");
   res.send(findUser);
 });
@@ -60,53 +97,51 @@ app.get("/api/products", (req, res) => {
 });
 
 //post request
-app.post("/api/users", (req, res) => {
-  console.log(req.body);
-  const { body } = req;
-  const newusers = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
-  mockUsers.push(newusers);
-  return res.status(201).send(newusers);
-});
+//validators----
+app.post(
+  "/api/users",
+  [
+    body("username")
+      .notEmpty()
+      .withMessage("User name cant be empty")
+      .isLength({ min: 5, max: 32 })
+      .withMessage("username must be atleast 5 to 32 characters")
+      .isString()
+      .withMessage("Username must be String"),
+    body("email").notEmpty().withMessage("Email cant be Empty"),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    console.log(result);
+    console.log(req.body);
+    if (!result.isEmpty())
+      return res.status(400).send({ errors: result.array() });
+    const { body } = req;
+    const newusers = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+    mockUsers.push(newusers);
+    return res.status(201).send(newusers);
+  }
+);
 
 //put request
 //to update whole data
-app.put("/api/users/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-  const parseId = parseInt(id);
-  if (isNaN(parseId)) return res.status(400).send("Enter correct Id!");
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parseId);
-  if (findUserIndex === -1) return res.status(404).send("User not found");
-  mockUsers[findUserIndex] = { id: parseId, ...body };
+app.put("/api/users/:id", resolveUserById, (req, res) => {
+  const { body, findUserIndex } = req;
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
   return res.status(200).send("User! Updated");
 });
 
 //patch request
 //partial updata of user data
-app.patch("/api/users/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-  const parseId = parseInt(id);
-  if (isNaN(parseId)) return res.status(400).send("Enter correct Id!");
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parseId);
-  if (findUserIndex === -1) return res.status(404).send("User not found");
+app.patch("/api/users/:id", resolveUserById, (req, res) => {
+  const { body, findUserIndex } = req;
   mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
   return res.status(200).send("User! Updated");
 });
 
 //delete request
-app.delete("/api/users/:id", (req, res) => {
-  const {
-    params: { id },
-  } = req;
-  const parseId = parseInt(id);
-  if (isNaN(parseId)) return res.status(400).send("Enter correct Id!");
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parseId);
-  if (findUserIndex === -1) return res.status(404).send("User not found");
+app.delete("/api/users/:id", resolveUserById, (req, res) => {
+  const { findUserIndex } = req;
   mockUsers.splice(findUserIndex, 1);
   return res.status(200).send("User deleted");
 });
